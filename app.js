68d7a70de0ff4d2033ee34a0333c3d7132b9f37e
noderguy <36875984+noderguy@users.noreply.github.com>
2018-02-27 23:22:01 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const pug = require('pug');
 const userRoutes = require('./routes/user-routes');
+const bodyParser = require('body-parser');
+const keys = require('../config/keys');
+const mongoose = require('mongoose');
 
 const app = express();
 
 
+// connect to mongoDB
+mongoose.connect(keys.dbURL);
+
+// bodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true });
+	
 // set up view engine 'pug'
 app.set('view engine', 'pug');
+
 // set up the path for static files like css / js
 app.use('/public', express.static('public'));
 
+// render main page
 app.get('/', (req, res)=>{
 	res.render('index');
 })
-app.get('/profile', userRoutes);
+// Routing
+app.use(userRoutes);
 
 // app server
 app.listen(8000, (req, res)=>{
