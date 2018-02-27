3dd216a144da4219848b405ebc4d9c680e4de02a
noderguy <36875984+noderguy@users.noreply.github.com>
2018-02-27 23:31:08 +0900

    Update user-routes.js

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,7 +1,34 @@
 const router = require('express').Router();
+const User = require('../models/user-model');
 
-router.get('/', (req, res)=>{
-	res.render('/profile');
+
+
+// Sign up!
+router.post('/profile', (req, res)=>{
+	
+	User.findOne({email:req.body.email}, (err, user){
+		     
+		if(err) throw err;
+		if(user){
+			res.redirect('/');
+			console.log('already exists');
+		}else{
+			const newUser = new User({
+				user.email : req.body.email,
+				user.password : req.body.password
+			}).save((err, newUser)=>{
+				if(err) throw err;
+				res.render('profile', {
+					email: 'welcome!' + req.body.email	
+				});
+			})
+		}
+	})
 })
+
+
+// Log in!
+
+// Log out!
 
 module.exports = router;
